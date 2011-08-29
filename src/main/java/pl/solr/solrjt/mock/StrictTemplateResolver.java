f36d117ebb241b45961a91c0b5238438349a4356
Marek Rogoziński <m.rogozinski@solr.pl>
2011-08-29 17:18:34 +0200

    support for update request

--- src/main/java/pl/solr/solrjt/mock/StrictTemplateResolver.java
+++ src/main/java/pl/solr/solrjt/mock/StrictTemplateResolver.java
@@ -2,11 +2,18 @@ package pl.solr.solrjt.mock;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.solr.client.solrj.SolrRequest;
+import org.apache.solr.client.solrj.request.UpdateRequest;
 
 public class StrictTemplateResolver implements TemplateResolver {
 
 	public String resolve(final SolrRequest req) {
-		return DigestUtils.md5Hex(req.getParams().toString()) + ".xml";
+		String fileBase;
+		if (req instanceof UpdateRequest) {
+			fileBase = ((UpdateRequest) req).getDocuments().toString();
+		} else {
+			fileBase = req.getParams().toString();
+		}
+		return DigestUtils.md5Hex(fileBase) + ".xml";
 	}
 
 }
